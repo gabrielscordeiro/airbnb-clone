fed1aa79171ff2fd40400fbec04fef3a66238424
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-09 00:13:04 -0300

    feat: 🎸 Create country select

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -21,7 +21,41 @@ const CountrySelect = ({ value, onChange }: CountrySelectProps) => {
 
     return (
         <div>
+            <Select
+                placeholder="Anywhere"
+                isClearable
+                options={getAll()}
+                value={value}
+                onChange={(value) => onChange(value as CountrySelectValue)}
+                formatOptionLabel={(option:any) => (
+                    <div className="flex flex-row items-center gap-3">
+                        <div>
+                            {option.flag}
+                        </div>
+                        <div>
+                            {option.label},
+                            <span className="text-neutral-500 ml-1">
+                                {option.region}
+                            </span>
+                        </div>
+                    </div>
+                )}
 
+                classNames={{
+                    control: () => 'p-3 border-2',
+                    input: () => 'text-lg',
+                    option: () => 'text-lg'
+                }}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 6,
+                    colors: {
+                        ...theme.colors,
+                        primary: 'black',
+                        primary25: '#ffe4e6'
+                    }
+                })}
+            />
         </div>
     )
 }
