ed259fdbe273dfdd9b5466b3356b7586c1cd4e37
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-08-11 23:38:36 -0300

    style: 💄 Listing hotels in a row

--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,15 @@ export default async function Home() {
   return (
     <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            <div>
-                {listings.map((listing: any) => {
-                    return (
-                        <ListingCard
-                            currentUser={currentUser}
-                            key={listing.id}
-                            data={listing}
-                        />
-                    )
-                })}
-            </div>
+            {listings.map((listing: any) => {
+                return (
+                    <ListingCard
+                        currentUser={currentUser}
+                        key={listing.id}
+                        data={listing}
+                    />
+                )
+            })}
         </div>
     </Container>
   )
